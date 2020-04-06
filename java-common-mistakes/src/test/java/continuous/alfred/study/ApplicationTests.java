71d9471972fa0b3e9a714ef9b7d412d27ee01d94
贤二 <yuanxun@gmail.com>
2020-04-06 22:03:11 +0800

    feat(java-common-mistakes) 并发工具类库，线程安全

---
+++ java-common-mistakes/src/test/java/continuous/alfred/study/ApplicationTests.java
@@ -0,0 +1,13 @@
+package continuous.alfred.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
