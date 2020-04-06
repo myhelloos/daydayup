85904bfc943fa34235fd80909d7d8667651af6c9
贤二 <yuanxun@gmail.com>
2020-04-06 22:56:43 +0800

    feat(code format) apply the spotless google java style

--- java-common-mistakes/src/main/java/continuous/alfred/study/Application.java
+++ java-common-mistakes/src/main/java/continuous/alfred/study/Application.java
@@ -3,14 +3,12 @@ package continuous.alfred.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import continuous.alfred.study.commons.Utils;
-
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-//        Utils.loadPropertySource(Application.class, "/tomcat.properties");
+  public static void main(String[] args) {
+    //        Utils.loadPropertySource(Application.class, "/tomcat.properties");
 
-        SpringApplication.run(Application.class, args);
-    }
+    SpringApplication.run(Application.class, args);
+  }
 }
--- java-common-mistakes/src/main/java/continuous/alfred/study/conurrenttool/concurrenthashmapperformance/ConcurrentHashMapPerformanceController.java
+++ java-common-mistakes/src/main/java/continuous/alfred/study/conurrenttool/concurrenthashmapperformance/ConcurrentHashMapPerformanceController.java
@@ -1,11 +1,5 @@
 package continuous.alfred.study.conurrenttool.concurrenthashmapperformance;
 
-import org.springframework.util.Assert;
-import org.springframework.util.StopWatch;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinPool;
@@ -14,87 +8,91 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/concurrenthashmapperformance")
 @Slf4j
 public class ConcurrentHashMapPerformanceController {
-    private static int LOOP_COUNT = 10000000;
-    private static int THREAD_COUNT = 10;
-    private static int ITEM_COUNT = 10;
+  private static int LOOP_COUNT = 10000000;
+  private static int THREAD_COUNT = 10;
+  private static int ITEM_COUNT = 10;
 
-    @GetMapping("/good")
-    public String good() throws InterruptedException {
-        StopWatch stopWatch = new StopWatch();
+  @GetMapping("/good")
+  public String good() throws InterruptedException {
+    StopWatch stopWatch = new StopWatch();
 
-        stopWatch.start("normaluse");
-        Map<String, Long> normaluse = normaluse();
-        stopWatch.stop();
-        Assert.isTrue(normaluse.size() == ITEM_COUNT, "normaluse size error");
-        Assert.isTrue(
-                normaluse.entrySet()
-                        .stream()
-                        .mapToLong(item -> item.getValue())
-                        .reduce(0, Long::sum) == LOOP_COUNT
-                , "normaluse count error");
+    stopWatch.start("normaluse");
+    Map<String, Long> normaluse = normaluse();
+    stopWatch.stop();
+    Assert.isTrue(normaluse.size() == ITEM_COUNT, "normaluse size error");
+    Assert.isTrue(
+        normaluse.entrySet().stream().mapToLong(item -> item.getValue()).reduce(0, Long::sum)
+            == LOOP_COUNT,
+        "normaluse count error");
 
-        stopWatch.start("gooduse");
-        Map<String, Long> gooduse = gooduse();
-        stopWatch.stop();
-        Assert.isTrue(gooduse.size() == ITEM_COUNT, "gooduse size error");
-        Assert.isTrue(
-                gooduse.entrySet()
-                        .stream()
-                        .mapToLong(item -> item.getValue())
-                        .reduce(0, Long::sum) == LOOP_COUNT
-                , "gooduse count error");
+    stopWatch.start("gooduse");
+    Map<String, Long> gooduse = gooduse();
+    stopWatch.stop();
+    Assert.isTrue(gooduse.size() == ITEM_COUNT, "gooduse size error");
+    Assert.isTrue(
+        gooduse.entrySet().stream().mapToLong(item -> item.getValue()).reduce(0, Long::sum)
+            == LOOP_COUNT,
+        "gooduse count error");
 
-        log.info(stopWatch.prettyPrint());
-        return "OK\n" + stopWatch.prettyPrint();
-    }
+    log.info(stopWatch.prettyPrint());
+    return "OK\n" + stopWatch.prettyPrint();
+  }
 
-    private Map<String, Long> normaluse() throws InterruptedException {
-        ConcurrentHashMap<String, Long> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
-        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+  private Map<String, Long> normaluse() throws InterruptedException {
+    ConcurrentHashMap<String, Long> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
+    ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
 
-        forkJoinPool.execute(() -> IntStream.rangeClosed(1, LOOP_COUNT)
+    forkJoinPool.execute(
+        () ->
+            IntStream.rangeClosed(1, LOOP_COUNT)
                 .parallel()
-                .forEach(i -> {
-                    String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
+                .forEach(
+                    i -> {
+                      String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
 
-                    synchronized (freqs) {
+                      synchronized (freqs) {
                         if (freqs.containsKey(key)) {
-                            freqs.put(key, freqs.get(key) + 1);
+                          freqs.put(key, freqs.get(key) + 1);
                         } else {
-                            freqs.put(key, 1L);
+                          freqs.put(key, 1L);
                         }
-                    }
-                }));
-        forkJoinPool.shutdown();
-        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+                      }
+                    }));
+    forkJoinPool.shutdown();
+    forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
 
-        return freqs;
-    }
+    return freqs;
+  }
 
-    private Map<String, Long> gooduse() throws InterruptedException {
-        ConcurrentHashMap<String, LongAdder> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
-        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+  private Map<String, Long> gooduse() throws InterruptedException {
+    ConcurrentHashMap<String, LongAdder> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
+    ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
 
-        forkJoinPool.execute(() -> IntStream.rangeClosed(1, LOOP_COUNT)
+    forkJoinPool.execute(
+        () ->
+            IntStream.rangeClosed(1, LOOP_COUNT)
                 .parallel()
-                .forEach(i -> {
-                    String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
+                .forEach(
+                    i -> {
+                      String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
 
-                    freqs.computeIfAbsent(key, k -> new LongAdder()).increment();
-                }));
-        forkJoinPool.shutdown();
-        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+                      freqs.computeIfAbsent(key, k -> new LongAdder()).increment();
+                    }));
+    forkJoinPool.shutdown();
+    forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
 
-        return freqs.entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey
-                        , e -> e.getValue().longValue()));
-    }
+    return freqs.entrySet().stream()
+        .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().longValue()));
+  }
 }
